6a1da39736e6f17a9ed7324a86f80e9e156e3f20
= <miguelcastrejongal@gmail.com>
2024-10-06 20:22:23 -0600

    Added all serializer for our models.

--- LittleLemon/LittleLemonRestaurant/serializers.py
+++ LittleLemon/LittleLemonRestaurant/serializers.py
@@ -1,10 +1,40 @@
 from rest_framework import serializers 
-from .models import MenuItem
+from .models import MenuItem, Category, Cart, Order
 from rest_framework.validators import UniqueTogetherValidator 
 from django.contrib.auth.models import User 
 
 
 class MenuItemSerializer(serializers.ModelSerializer):
+    category = serializers.StringRelatedField(read_only=True)
+    category_id = serializers.IntegerField(write_only=True)
+    
     class Meta:
         model = MenuItem
-        fields = ['id', 'title', 'price', 'category', 'featured']
+        fields = ['id', 'title', 'price', 'featured', 'category', 'category_id']
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+        read_only_fields = ['id']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        exclude = ['password', 'user_permissions']
+
+
+class CartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cart
+        fields = ['id', 'user', 'quantity', 'unit_price', 'price', 'menuitem']
+        read_only_fields = ['user', 'unit_price', 'price']
+
+
+class OrdersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = '__all__'
+        read_only_fields = ['total', 'user']
